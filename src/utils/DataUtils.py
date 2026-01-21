fd5612932b80dd951b832b55b8c33d3fbf80084c
GSteve12 <gwadesteve12@gmail.com>
2026-01-21 23:30:47 +0100

    Implemented priority sampling in datautils

--- src/utils/DataUtils.py
+++ src/utils/DataUtils.py
@@ -57,9 +57,9 @@ def set_seeds(seed=42):
     
 # Worker seed function for DataLoader 
 def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2**32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
+    worker_seed = torch.initial_seed() % 2**32 
+    np.random.seed(worker_seed) 
+    random.seed(worker_seed) 
 
 try:
     import albumentations as A
